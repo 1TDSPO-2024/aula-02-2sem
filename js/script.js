93616eaf3d0cb64be396b8d550064f2be3f24190
SunaUezuri <wesley.jane.santos@gmail.com>
2024-08-08 23:05:16 -0300

    Utilização do MAP

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
+//Criando um array de objetos de alunos
+let alunos = [
+    {id:1, nome: "Vanessa", rm:12345},
+    {id:2, nome: "Nicolas", rm:78901},
+    {id:3, nome: "Jonas", rm:12567},
+    {id:4, nome: "Giulia", rm:75434},
+    {id:5, nome: "Frederico", rm:92345},
+]
 
+const nomesAlunos = alunos.map( nome => nome.nome);
+
+console.log(nomesAlunos);
+
+let notas = [
+    {id:1, nota:8},
+    {id:2, nota:7.5},
+    {id:3, nota:6.7},
+    {id:4, nota:4},
+    {id:5, nota:2},
+]
+
+const notasAlunos = alunos.map(aluno => {
+    const nota = notas.find( n => n.id === aluno.id )
+    return {...aluno,nota:nota.nota};
+})
+
+console.log(notasAlunos);
+
+const somaNotas = notasAlunos.reduce( (acumulador,notasAlunos)=>{
+    return acumulador + notasAlunos.nota;
+},0 )
+
+console.log(notasAlunos);
