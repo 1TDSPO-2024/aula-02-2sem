bb43172d2a20f520e0a0612ee5755e3b5fc633ab
GuiJanunzzi <guijanunzzi7@gmail.com>
2024-08-08 23:15:13 -0300

    aula02 map e reduce

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
 
+//Criando um array de objeto de alunos
+let alunos = [
+    {id:1,nome:"Vanessa",rm:12345},
+    {id:2,nome:"Nicolas",rm:78901},
+    {id:3,nome:"Jonas",rm:12567},
+    {id:4,nome:"Giulia",rm:75434},
+    {id:5,nome:"Frederico",rm:92345}
+];
+
+const nomesAlunos = alunos.map(nome => nome.nome);
+console.log(nomesAlunos);
+
+//Criando um array de notas
+let notas = [
+    {id:1,nota:8},
+    {id:2,nota:7.5},
+    {id:3,nota:6.7},
+    {id:4,nota:4},
+    {id:5,nota:2}
+];
+
+// const notasAlunos = alunos.map(aluno =>{
+//     const nota = notas.find( n => n.id == aluno.id );
+//     return {id:aluno.id,nome:aluno.nome,rm:aluno.rm,nota:nota.nota }
+// })
+
+const notasAlunos = alunos.map(aluno =>{
+    const nota = notas.find( n => n.id == aluno.id );
+    return {...aluno,nota:nota.nota }
+})
+
+console.log(notasAlunos)
+
+const somaNotas = notasAlunos.reduce( (acumulador,notasAlunos)=>{
+    return acumulador + notasAlunos.nota;
+},0)
+
+console.log(somaNotas)
