3602cbbfaa8efeff68f05183e1bbe55ae68ec60c
raphatatto <raphaelatatto@gmail.com>
2024-08-08 23:11:03 -0300

    add js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-
+let alunos = [
+    {id:1, nome:"Vanessa",rm:12345},
+    {id:2, nome:"Nicolas",rm:78901},
+    {id:3, nome:"Jonas",rm:12567},
+    {id:4, nome:"Giulia",rm:75434},
+    {id:5, nome:"Frederico",rm:92345},
+];
